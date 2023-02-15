0bd71cd871ace177ae2cb94e436ed1df3433c831
Tyler Jefferson <tylerrjefferson00@gmail.com>
2023-02-15 16:57:34 -0500

    hero section added with button linking to email

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,21 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello World! <br />
+        Welcome To My Portfolio
+      </SectionTitle>
+      <SectionText>
+        My name is Tyler Jefferson, I am primarily a MERN Stack JavaScript developer. I have a lot of love for coding and strive to learn new languages and technologies whenever the opportunity presents itself!
+      </SectionText>
+      <a href="mailto:TylerRJefferson00@iCloud.com">
+        <Button>Email Me</Button>
+      </a>
+
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
